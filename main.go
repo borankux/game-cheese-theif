b105be8a749a63db9270b628f8563b366297c051
Frank <mablat@ebay.com>
2022-01-07 15:35:48 +0800

    update room

--- main.go
+++ main.go
@@ -3,15 +3,32 @@ package main
 import (
 	"cheese-theif/id"
 	"fmt"
+	"github.com/fatih/color"
 	"github.com/gin-gonic/gin"
 	socketio "github.com/googollee/go-socket.io"
 	"github.com/googollee/go-socket.io/engineio"
 	"log"
 	"net/http"
+	"runtime"
 	"sync"
+	"time"
 )
 
 var tokenMap = sync.Map{}
+
+type Stat struct {
+	Rooms     int      `json:"rooms"`
+	Users     int      `json:"users"`
+	Total     int      `json:"total"`
+	RoomNames []string `json:"room_names"`
+}
+
+type MemoryStat struct {
+	Alloc      string `json:"alloc"`
+	TotalAlloc string `json:"total_alloc"`
+	Sys        string `json:"sys"`
+	NumGC      string `json:"num_gc"`
+}
 
 func haveUser(token string) bool {
 	if _, ok := tokenMap.Load(token); ok {
@@ -20,6 +37,37 @@ func haveUser(token string) bool {
 	return false
 }
 
+func updateInfo(server *socketio.Server) {
+	var rooms []string
+	tokenMap.Range(func(key, value interface{}) bool {
+		fmt.Println(key, value)
+		rooms = append(rooms, key.(string))
+		return true
+	})
+
+	server.BroadcastToNamespace("/stats", "update", Stat{
+		Rooms:     len(server.Rooms("/game")),
+		Users:     len(rooms),
+		Total:     server.Count(),
+		RoomNames: rooms,
+	})
+}
+
+func bToMb(b uint64) uint64 {
+	return b / 1024 / 1024
+}
+
+func getStats() MemoryStat {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+
+	return MemoryStat{
+		Alloc:      fmt.Sprintf("Alloc = %v MiB", bToMb(m.Alloc)),
+		TotalAlloc: fmt.Sprintf("TotalAlloc = %v MiB", bToMb(m.TotalAlloc)),
+		Sys:        fmt.Sprintf("Sys = %v MiB", bToMb(m.Sys)),
+		NumGC:      fmt.Sprintf("NumGC = %v", m.NumGC),
+	}
+}
 
 func main() {
 	serverOptions := engineio.Options{
@@ -28,24 +76,41 @@ func main() {
 
 	server := socketio.NewServer(&serverOptions)
 	server.OnConnect("/", func(conn socketio.Conn) error {
-		url := conn.URL()
-		token := url.Query().Get("token")
-		if !haveUser(token) {
-			conn.Emit("/token")
-		}
-		userID := conn.ID()
-		fmt.Printf("connected:%s\n", userID)
+		conn.SetContext("")
+		updateInfo(server)
+		color.Green("root:client connected: %s\n", conn.ID())
+		return nil
+	})
+	server.OnDisconnect("/", func(conn socketio.Conn, s string) {
+		color.Red("disconnected:%s\n%s\n", conn.ID(), s)
+	})
+
+	server.OnConnect("/stats", func(conn socketio.Conn) error {
+		updateInfo(server)
+		fmt.Printf("/stats:client connected: %s\n", conn.ID())
 		return nil
 	})
 
-	server.OnDisconnect("/", func(conn socketio.Conn, reason string) {
-		userID := conn.ID()
-		fmt.Printf("disconnected:%s, reason:%s\n", userID, reason)
+	server.OnDisconnect("/stats", func(conn socketio.Conn, s string) {
+		fmt.Printf("/stats:client disconnected:%s\n", conn.ID())
 	})
 
-	server.OnEvent("/", "msg", func(s socketio.Conn, msg string) {
-		fmt.Printf("%s, %s\n", msg, s.ID())
-		server.BroadcastToNamespace("/", "broadcast", fmt.Sprintf("%s says:%s", s.ID(), msg))
+	server.OnConnect("/game", func(conn socketio.Conn) error {
+		updateInfo(server)
+		color.Green("/game: connected:%s", conn.ID())
+		return nil
+	})
+
+	go func() {
+		for {
+			time.Sleep(time.Second * 5)
+			server.BroadcastToNamespace("/stats", "memory", getStats())
+		}
+	}()
+
+	server.OnDisconnect("/game", func(conn socketio.Conn, s string) {
+		updateInfo(server)
+		color.Red("/game:disconnected:%s", conn.ID())
 	})
 
 	go func() {
